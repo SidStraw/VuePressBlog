3a7d4f2c8f52b80aacca353b6db863789c6cf05c
Sid-Straw <j80304xz@gmail.com>
2020-10-28 22:22:47 +0800

    feat: build前更新Stylus變數設定

---
+++ renewPaletteStylus.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const YAML = require('yaml')
+
+const defaultColor = { $accentColor: '#99B7A3', $secondaryColor: '#84969C' }
+const fileStylus = fs.readFileSync('./.vuepress/styles/palette.styl', 'utf8')
+const file = fs.readFileSync('./.vuepress/config.yml', 'utf8')
+const { themeConfig } = YAML.parse(file)
+const { color } = themeConfig
+if (color && Object.keys(color).some(key => !!color[key])) {
+  const stylusContent = Object.keys(color)
+    .map(key => `${key} = ${color[key] || defaultColor[key]};`)
+    .join('\n')
+
+  fs.writeFileSync('./.vuepress/styles/palette.styl', fileStylus + '\n' + stylusContent)
+}
+
+console.log('palette.styl 更新完成')
