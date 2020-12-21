aae8c83ec68399241b4dc3a642d3a39468485714
Sid-Straw <j80304xz@gmail.com>
2020-12-21 23:08:32 +0800

    refactor: SlideGroups>增加自動滾動

--- .vuepress/theme/components/SlideGroups.vue
+++ .vuepress/theme/components/SlideGroups.vue
@@ -1,9 +1,9 @@
 <template>
-  <v-sheet class="mx-auto" elevation="8">
+  <v-sheet class="mx-auto" elevation="1">
     <h2 class="pt-5 px-5 text-center primary--text">{{ title }}</h2>
-    <v-slide-group v-model="model" class="pa-4" show-arrows center-active>
-      <v-slide-item v-for="p in portfolio" :key="p.key">
-        <v-card class="ma-4" height="200" width="300" :to="itemLink && p.path">
+    <v-slide-group v-model="model" class="pa-4" show-arrows center-active @change="disableCycle">
+      <v-slide-item v-for="p in portfolio" :key="p.key" v-slot:default="{ active, toggle }">
+        <v-card class="ma-4" height="200" width="300" :to="itemLink ? p.path : ''" @click="toggle">
           <v-img
             :src="p.frontmatter.image"
             class="white--text align-end fill-height"
@@ -29,9 +29,18 @@ export default {
       type: Boolean,
       default: false,
     },
+    cycle: {
+      type: Boolean,
+      default: false,
+    },
+    interval: {
+      type: Number,
+      default: 3000,
+    },
   },
   data: () => ({
-    model: null,
+    model: 0,
+    intervalInstance: null,
   }),
   computed: {
     portfolio() {
@@ -44,6 +53,22 @@ export default {
           if (aDate > bDate) return -1
           return 0
         })
+    },
+  },
+  mounted() {
+    if (this.cycle) this.intervalInstance = setInterval(this.autoPlay, this.interval)
+  },
+  beforeDestroy() {
+    this.disableCycle()
+  },
+  methods: {
+    autoPlay() {
+      if (typeof this.model === 'number') {
+        this.model = this.model <= this.portfolio.length ? this.model + 1 : 0
+      }
+    },
+    disableCycle() {
+      clearInterval(this.intervalInstance)
     },
   },
 }
