9b0d2577439798628d08a45c6aad8bb31a7814e7
SidStraw <j80304xz@gmail.com>
2022-08-29 18:08:48 +0800

    ci: add Cloudflare Pages Deployed

---
+++ .github/get-cloudflare-page-url.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const JSDOM = require('jsdom').JSDOM
+
+const [html] = process.argv.slice(2)
+const { document } = new JSDOM(html).window
+
+const linkElement = document.querySelector('a')
+console.log(linkElement?.href ? linkElement.href : 'Build failed')
